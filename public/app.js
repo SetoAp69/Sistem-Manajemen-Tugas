59c93033148d653e23425cbad32cab834f66bccb
DestaHutamaP <destahp10@gmail.com>
2023-11-29 21:52:42 +0700

    Adding Design Into Website

--- public/app.js
+++ public/app.js
@@ -1,7 +1,6 @@
 
 import {getDatabase, ref, set,onValue,update} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-
 import { signInWithPopup, signInWithRedirect,getRedirectResult, GoogleAuthProvider ,getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,browserSessionPersistence,onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 // import * as firebase from'firebase/app';
 // import 'firebase/auth'
@@ -17,32 +16,115 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 
+const auth = getAuth();
 
+if (document.getElementById('index')) {
+    const auth = getAuth();
 
-
-if(document.getElementById('index')){
-    const auth=getAuth()
-
-    onAuthStateChanged(auth,function(user){
-        if(user){
+    onAuthStateChanged(auth, function (user) {
+        if (user) {
+            // User is authenticated
             console.log(user.uid);
-            document.getElementById('username').textContent="Hello, \n"+user.displayName
-            getTask()
-            
-            
-        }else{
+            document.getElementById('username').textContent = "Hello, \n" + user.displayName;
+            getTask();
+        } else {
+            // User is not authenticated
             console.log('not logged in yet');
-            window.location.href='login.html';
+
+            // Check the current location
+            const currentLocation = window.location.pathname;
+
+            // If the user tries to access manajemenTask.html, redirect to login.html
+            if (currentLocation.includes('manajemenTask.html')) {
+                window.location.href = 'login.html';
+            } else {
+                // If the user is on any other page, redirect to index.html
+                window.location.href = 'index.html';
+            }
         }
-    })
+    });
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const userDropdown = document.getElementById('userDropdown');
+    const loginOption = document.getElementById('loginOption');
+    const registerOption = document.getElementById('registerOption');
+    const logoutOption = document.getElementById('logoutOption');
+    const auth = getAuth();
+
+    const loginButton = document.getElementById('loginButton');
+    if (loginButton) {
+        loginButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            emailAndPassLogin(email, password);
+        });
+    }
+
+    const linkGoToRegister = document.getElementById('link-goToRegister');
+    if (linkGoToRegister) {
+        linkGoToRegister.addEventListener("click", function (event) {
+            event.preventDefault();
+            window.location.href = 'register.html';
+        });
+    }
+
+    const googleSignInButton = document.getElementById('googleSignInButton');
+    if (googleSignInButton) {
+        googleSignInButton.addEventListener("click", loginWithEmail);
+    }
+  
+    onAuthStateChanged(auth, function (user) {
+        console.log('User state changed:', user);
+  
+        if (user) {
+            // User is authenticated
+            console.log('User is logged in:', user.email);
+            if (userDropdown) {
+                userDropdown.querySelector('.dropbtn').textContent = user.email;
+            }
+            if (loginOption) {
+                loginOption.style.display = 'none';
+            }
+            if (registerOption) {
+                registerOption.style.display = 'none';
+            }
+            if (logoutOption) {
+                logoutOption.style.display = 'block';
+            }
+            getTask(); 
+        } else {
+            // User is not authenticated
+            console.log('User is logged out');
+            if (userDropdown) {
+                userDropdown.querySelector('.dropbtn').textContent = 'User';
+            }
+            if (loginOption) {
+                loginOption.style.display = 'block';
+            }
+            if (registerOption) {
+                registerOption.style.display = 'block';
+            }
+            if (logoutOption) {
+                logoutOption.style.display = 'none';
+            }
+        }
+    });
+  
+    if (logoutOption) {
+        logoutOption.addEventListener('click', function (event) {
+            event.preventDefault();
+            logOut();
+        });
+    }
+});
 
 
 function logOut(){
     const auth = getAuth();
     signOut(auth).then(()=>{
-        window.location.href='login.html'
+        window.location.href='index.html'
     }).catch((error)=>{
         console.log(error.message)
     });
@@ -50,19 +132,21 @@ function logOut(){
 }
 
   
-function emailAndPassLogin(email1,password1){
-    const email=email1;
-    const password=password1;
-    const auth=getAuth();
-    signInWithEmailAndPassword(auth,email,password)
-    .then((userCredential)=>{
-        const user=userCredential.user;
-        window.location.href='index.html'
-    })
-    .catch((error)=>{
-        const errorCode=error.code;
-        const errorMessage=error.message;
-    });
+function emailAndPassLogin(email1, password1) {
+    const email = email1;
+    const password = password1;
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            // Redirect after successful login
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
 }
 
 // document.addEventListener("DOMContentLoaded", event=>{
@@ -81,97 +165,110 @@ function emailAndPassLogin(email1,password1){
 //     }
 // }
 
-function getTask(){
-    const auth=getAuth();
-    const user=auth.currentUser;
-    let lateTask=[]
-    let task=[]
-    console.log(user);
-    if(user){
-        
-        const db=getDatabase()
-        const refx=ref(db,'users/'+user.uid+'/task/')
-        
-        onValue(refx, (snapshot)=>{
-            const data=snapshot.val()
-            
-            for(const taskData in data){
-                if(new Date(data[taskData].deadline)<new Date()){
-                    console.log(new Date(data[taskData].deadline)+'  < '+ new Date())
-                    lateTask.push(data[taskData])
-                    
-                }else{
-                    console.log(new Date(data[taskData].deadline)+'  > '+ new Date())
-                    task.push(data[taskData])
-                    
-                }
-            
-            }
-                const taskTable=createTable(task,'Task');
-                const lateTaskTable=createTable(lateTask,'Late Task');
-                          
-                console.log(task)
-                console.log(lateTask)
-                document.body.appendChild(taskTable)
-                document.body.appendChild(lateTaskTable)
+function getTask() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    let lateTask = [];
+    let task = [];
 
-        })  
-        
-        
-    }else{
-        console.log('user not found')
+    if (user) {
+        const db = getDatabase();
+        const refx = ref(db, 'users/' + user.uid + '/task/');
+
+        onValue(refx, (snapshot) => {
+            const data = snapshot.val();
+
+            // Clear existing tables
+            const leftTableContainer = document.getElementById('left-table');
+            const rightTableContainer = document.getElementById('right-table');
+            leftTableContainer.innerHTML = '';
+            rightTableContainer.innerHTML = '';
+
+            for (const taskData in data) {
+                if (new Date(data[taskData].deadline) < new Date()) {
+                    console.log(new Date(data[taskData].deadline) + '  < ' + new Date());
+                    lateTask.push(data[taskData]);
+                } else {
+                    console.log(new Date(data[taskData].deadline) + '  > ' + new Date());
+                    task.push(data[taskData]);
+                }
+            }
+
+            const leftTable = createTable(task);
+            const rightTable = createTable(lateTask);
+
+            leftTableContainer.appendChild(leftTable);
+            rightTableContainer.appendChild(rightTable);
+        });
+    } else {
+        console.log('user not found');
     }
-    
-   
-    
 }
 
-function createTable(tableData,tableName){
-    const table=document.createElement('table')
-    table.setAttribute('id','table')
-    const tableBody=document.createElement('tbody')
-    tableBody.setAttribute('id','tbody')
+  function createTable(tableData, tableName) {
+    const table = document.createElement('table');
+    table.setAttribute('class', 'table table-striped table-hover');
 
-    const tableCaption=document.createElement('caption')
-    tableCaption.textContent=tableName;
+    const thead = document.createElement('thead');
+    const headerRow = thead.insertRow();
+
+    // Create the header cells
+    const headers = ['No', 'Nama Tugas', 'Deadline', 'Done', 'Edit'];
+    headers.forEach((headerText) => {
+        const th = document.createElement('th');
+        th.textContent = headerText;
+        headerRow.appendChild(th);
+    });
+
+    table.appendChild(thead);
+
+    const tbody = document.createElement('tbody');
+    table.appendChild(tbody);
+
+    const tableCaption = document.createElement('caption');
+    tableCaption.textContent = tableName;
     table.appendChild(tableCaption);
-    console.log(tableData);
-    tableData.forEach(task=>{
-        const row=tableBody.insertRow();
-        row.insertCell().textContent=task.taskName;
-        row.insertCell().textContent=task.deadline;
-        
+
+    let counter = 1;
+
+    tableData.forEach((task) => {
+        const row = tbody.insertRow();
+
+        const counterCell = row.insertCell();
+        counterCell.textContent = counter++;
+
+        row.insertCell().textContent = task.taskName;
+        row.insertCell().textContent = task.deadline;
+
         const doneButtonCell = row.insertCell();
         const doneButton = document.createElement('button');
-        doneButton.textContent = 'Done'; // Change text accordingly
+        doneButton.textContent = 'Done';
         doneButton.setAttribute('data-task-id', task.taskId);
-        doneButton.addEventListener('click',function(event){
-            event.preventDefault()
-            try{
-                deleteTask(task.taskId)
-                window.location.href='index.html'
-
-            }catch(error){
-                console.log(error)
+        doneButton.setAttribute('class', 'btn btn-success');
+        doneButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            try {
+                deleteTask(task.taskId);
+                window.location.href = 'manajemenTask.html';
+            } catch (error) {
+                console.log(error);
             }
-            
-        }) // Optionally set data attributes
+        });
         doneButtonCell.appendChild(doneButton);
 
         const editButtonCell = row.insertCell();
         const editButton = document.createElement('button');
-        editButton.textContent = 'Edit'; // Change text accordingly
-        editButton.id='taskEditButton'
-        // editButton.setAttribute('href',`editTask.html?taskID=${task.taskId}`)
-        editButton.addEventListener('click',function(event){
-            event.preventDefault()
-            window.location.href=`editTask.html?taskID=${task.taskId}`
-        })
-        editButton.setAttribute('data-task-id', task.taskId); // Optionally set data attributes
+        editButton.textContent = 'Edit';
+        editButton.id = 'taskEditButton';
+        editButton.setAttribute('class', 'btn btn-warning'); // Bootstrap styling
+        editButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.location.href = `editTask.html?taskID=${task.taskId}`;
+        });
+        editButton.setAttribute('data-task-id', task.taskId);
         editButtonCell.appendChild(editButton);
-
     });
-    table.appendChild(tableBody);
+
     return table;
 }
 
@@ -292,6 +389,7 @@ export function registerUsingEmailAndPassword(email,password){
     createUserWithEmailAndPassword(auth, email,password)
     .then((userCredential)=>{
         const user = userCredential.user;
+        window.location.href = 'login.html';
     })
     .catch((error)=>{
         const errorCode=error.code;
@@ -394,7 +492,7 @@ if(document.getElementById('taskForm')){
 
          addNewTask(name,deadline,);
 
-         
+         window.location.href = 'manajemenTask.html';
          
     
      })
@@ -436,52 +534,76 @@ if(document.getElementById('taskForm')){
 
  
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const taskEditForm = document.getElementById('taskEditForm');
-    if(taskEditForm){
-        console.log('form loaded')
-        const app=initializeApp(firebaseConfig)
-        const queryString=window.location.search
+
+    if (taskEditForm) {
+        console.log('form loaded');
+        const app = initializeApp(firebaseConfig);
+        const queryString = window.location.search;
         const taskNameForm = document.getElementById('taskName');
         const deadlineForm = document.getElementById('deadline');
         const urlParams = new URLSearchParams(queryString);
-        const taskId=urlParams.get('taskID');
-        
-        
-        if(taskId){
-            const auth=getAuth();
-            onAuthStateChanged(auth,function(user){
-                if(user){
-                    console.log(user)
-                    const db=getDatabase()
-                    const refx=ref(db,'users/'+user.uid+'/task/'+taskId)
-                    
-                    onValue(refx, (snapshot)=>{
-                        const data=snapshot.val()
-                        taskNameForm.value=data.taskName;
-                        deadlineForm.value=data.deadline;
-            
-                    })  
+        const taskId = urlParams.get('taskID');
 
-                    document.getElementById('editSubmit').addEventListener('click',function(event){
-                        
-                        const updates={};
-                        const postData={
-                            taskName:taskNameForm.value,
-                            deadline:deadlineForm.value,
-                        }
-                        updates['/users/'+user.uid+'/task/'+taskId]=postData
-                        return update(ref(db),updates)
-                    })
-                }else{
-                    console.log('user not found')
+        let editSubmitButton;
+
+        if (taskId) {
+            const auth = getAuth();
+            onAuthStateChanged(auth, function (user) {
+                if (user) {
+                    console.log('user found:', user);
+                    const db = getDatabase();
+                    const refx = ref(db, 'users/' + user.uid + '/task/' + taskId);
+
+                    onValue(refx, (snapshot) => {
+                        const data = snapshot.val();
+                        taskNameForm.value = data.taskName;
+                        deadlineForm.value = data.deadline;
+
+                        console.log('taskId:', taskId);
+                    });
+                } else {
+                    console.log('user not found');
                 }
-            })
+            });
 
-           
-            
+            // Define editSubmitButton outside the onAuthStateChanged callback
+            editSubmitButton = document.getElementById('editSubmit');
+        } else {
+            console.log('taskId is undefined or null');
+        }
+
+        // Check if editSubmitButton is not null before attaching the event listener
+        if (editSubmitButton) {
+            editSubmitButton.addEventListener('click', function (event) {
+                event.preventDefault();
+
+                const auth = getAuth();
+                const user = auth.currentUser;
+                const db = getDatabase();
+
+                const updates = {};
+                const postData = {
+                    taskName: taskNameForm.value,
+                    deadline: deadlineForm.value,
+                };
+                updates['/users/' + user.uid + '/task/' + taskId] = postData;
+
+                console.log('Updating taskId:', taskId);
+
+                // Update the database
+                update(ref(db), updates)
+                    .then(() => {
+                        // Redirect after successful update
+                        window.location.href = 'manajemenTask.html';
+                    })
+                    .catch((error) => {
+                        console.log(error.message);
+                    });
+            });
         }
     } else {
-        console.log('not loaded')
+        console.log('not loaded');
     }
 });
